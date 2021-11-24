35033bff9ea760cfde59fb8b831a8b5db9779563
Tiago CARVALHO MARTINS <carvalhomartins.tiago@gmail.com>
2021-11-24 19:09:12 +0100

    Creation of success event
    Modification of html for creating the success page, modification of CSS for the same thing. adding JS for the success event

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelectorAll(".close");
+const submitBtn = document.querySelectorAll (".btn-submit");
+const successPg = document.querySelectorAll(".bground--success");
+const closeSuccessBtn = document.querySelectorAll(".btn-close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +24,27 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close form event
+closeForm.forEach((cross) =>cross.addEventListener("click", closeFormular));
 
+//close formular
+function closeFormular() {
+  modalbg.style.display = "none";
+}
+
+//submit form event
+submitBtn.forEach((submit) =>submit.addEventListener("click", openSuccess));
+
+//open success page
+function openSuccess() {
+  //modalbg.style.display = "none";
+  successPg.style.display = "block";
+}
+
+// close success event
+closeSuccessBtn.forEach((close) =>close.addEventListener("click", closeSuccess));
+
+//close success
+function closeSuccess() {
+  success.style.display = "none";
+}
