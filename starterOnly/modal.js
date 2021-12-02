18bb456f2652a271dc1a712dac284c942d8cf03f
Tiago CARVALHO MARTINS <carvalhomartins.tiago@gmail.com>
2021-12-02 11:27:27 +0100

    Form data

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,15 +13,8 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeForm = document.querySelectorAll(".close");
 const submitBtn = document.querySelectorAll (".btn-submit");
-const successPg = document.querySelector(".bground--success");
+const successPg = document.querySelectorAll(".bground--success");
 const closeSuccessBtn = document.querySelectorAll(".btn-close");
-//Form Data
-let firstName = document.getElementById("first");
-let lastName = document.getElementById("last").value;
-let email = document.getElementById("email").value;
-let birthDate = document.getElementById("birthdate").value;
-let eventQuantity = document.getElementById("quantity").value;
-//let cityEvent = document.getElementById("nom").value;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -31,31 +24,19 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//recuperation data from the form
-firstName.addEventListener('change', function() {
-  firstName = firstName.value
-});
-
-//input.addEventListener('change', updateValue);
-//input.addEventListener('change', updateValue);
-//input.addEventListener('change', updateValue);
-//input.addEventListener('change', updateValue);
-
 // close form event
 closeForm.forEach((cross) =>cross.addEventListener("click", closeFormular));
 
 //close formular
 function closeFormular() {
   modalbg.style.display = "none";
-  successPg.style.display = "none"
 }
 
 //submit form event
 submitBtn.forEach((submit) =>submit.addEventListener("click", openSuccess));
 
 //open success page
-function openSuccess(event) {
-  event.preventDefault();
+function openSuccess() {
   modalbg.style.display = "none";
   successPg.style.display = "block";
 }
@@ -65,5 +46,5 @@ closeSuccessBtn.forEach((close) =>close.addEventListener("click", closeSuccess))
 
 //close success
 function closeSuccess() {
-  successPg.style.display = "none";
+  success.style.display = "none";
 }
